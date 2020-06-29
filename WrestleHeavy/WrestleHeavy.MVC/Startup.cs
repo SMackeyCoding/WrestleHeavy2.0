37514bbc3fa86c891e158fa2d4797b521598c1df
SMackeyCoding <s.Mack.coding@gmail.com>
2020-06-29 17:56:31 -0400

    InitialCommit

---
+++ WrestleHeavy/WrestleHeavy.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WrestleHeavy.MVC.Startup))]
+namespace WrestleHeavy.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
